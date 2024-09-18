9d128462893c0decf8f764d2b9cce88798b1dfaa
Richi110899 <ricardo.gomez@tecsup.edu.pe>
2024-09-17 23:35:58 -0500

    Update WellnessViewModel.kt

--- app/src/main/java/com/example/estados_jet/WellnessViewModel.kt
+++ app/src/main/java/com/example/estados_jet/WellnessViewModel.kt
@@ -4,11 +4,6 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 
 class WellnessViewModel : ViewModel() {
-    /**
-     * Don't expose the mutable list of tasks from outside the ViewModel.
-     * Instead define _tasks and tasks. _tasks is internal and mutable inside the ViewModel.
-     * tasks is public and read-only.
-     */
     private val _tasks = getWellnessTasks().toMutableStateList()
     val tasks: List<WellnessTask>
         get() = _tasks
